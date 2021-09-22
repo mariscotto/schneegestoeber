d0564ef9e0ec2744ac577b65de21bda5246899de
Mariangela Scotto <mari.scottodicarlo@gmail.com>
2021-09-23 01:26:56 +0200

    added first door

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from "./logo.svg";
+import minus from "./img/minus.png";
+import plus from "./img/plus.png";
 import "./App.css";
 
 function App() {
   return (
     <>
+    <div id="grid">
       <h1 id="title">Schneegestöber</h1>
+
       <div class="top_row">
         <div class="inspiration_dice">
-          <img src="minus.jpg"></img>
+          <img src={minus} alt="minus" class="buttons"></img>
           <input type="number"></input>
-          <img src="plus.jpg"></img>
+          <img src={plus}  alt="plus" class="buttons"></img>
         </div>
         <button class="character">character sheet</button>
+      </div>
+
+      <div class="tuer">
+        <h3>1: The door</h3>
+      </div>
       </div>
     </>
   );
