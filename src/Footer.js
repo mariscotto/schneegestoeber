3afaf5c9f6fad07619e978e313512c1ca7ab6d79
Mariangela Scotto <mari.scottodicarlo@gmail.com>
2021-10-12 03:12:53 +0200

    dedication in footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div class="footer">
+      <p>Für eine wunderschöne und abenteuerliche Adventszeit mit dir &#9825;</p>
       <p>Copyright © 2021 Mariangela Scotto di Carlo</p>
       <p>
         <a
