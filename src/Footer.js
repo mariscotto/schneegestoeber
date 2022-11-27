4d894e6a85b607bb02991cd486cbc016ddf2a890
Mariangela Scotto <mari.scottodicarlo@gmail.com>
2022-11-27 23:30:40 +0100

    optimized responsiveness

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div class="footer">
       <p>Für eine wunderschöne und abenteuerliche Adventszeit mit dir &#9825;</p>
-      <p>Copyright © 2021 Mariangela Scotto di Carlo</p>
+      <p>Copyright © 2022 Mariangela Scotto di Carlo</p>
       <p>
         <a
           href="https://github.com/mariscotto/schneegestoeber"
